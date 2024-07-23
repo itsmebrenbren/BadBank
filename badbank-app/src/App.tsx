3d38d98165af756c37c55be4117977fe1b41c894
Brenna <brennalangille@Brennas-MBP.lan>
2024-07-23 11:38:15 -0400

    fixed protected routing issues

--- badbank-app/src/App.tsx
+++ badbank-app/src/App.tsx
@@ -3,10 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { useAuth } from './components/hooks/useAuth';
+import ProtectedRoute from './components/protectedRoute';
 import Menu from './components/navbar';
 import Home from './components/home';
 import Deposit from './components/deposit';
-// import Withdraw from './components/withdraw';
 import Login from './components/login';
 import CreateAccount from './components/createAccount';
 import Withdraw from './components/withdraw';
@@ -23,8 +23,8 @@ const App: React.FC = () => {
         <Menu isAuthenticated={isAuthenticated} />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/deposit" element={isAuthenticated ? <Deposit /> : <Login />}/>
-          <Route path="/withdraw" element={isAuthenticated ? <Withdraw /> : <Login />} />
+          <Route path="/deposit" element={<ProtectedRoute element={<Deposit />} />} />
+         <Route path="/withdraw" element={<ProtectedRoute element={<Withdraw />} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/createaccount" element={<CreateAccount />} />
         </Routes>
---
+++ badbank-app/src/components/protectedRoute.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from './hooks/useAuth';
+
+interface ProtectedRouteProps {
+  element: React.ReactElement;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
+  const { isAuthenticated } = useAuth();
+
+  return isAuthenticated ? element : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
+
+
+
+
